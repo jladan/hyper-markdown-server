8eb863e0b6b53a2bdcca906673fd9d0838e87a9d
John Ladan <john@ladan.ca>
2023-07-19 18:05:51 -0400

    add some state to the service function

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,28 @@
 use std::convert::Infallible;
 use std::net::SocketAddr;
+use std::sync::Arc;
+use std::sync::atomic::AtomicU64;
 
 use hyper::{Method, StatusCode, Body, Request, Response, Server};
 use hyper::service::{make_service_fn, service_fn};
 
-use futures::{TryStreamExt as _, Stream};
-
 #[tokio::main]
 async fn main() {
     let addr = SocketAddr::from(([0,0,0,0], 7878));
+    let state = Arc::new(ServerState { counter: AtomicU64::new(0) });
 
     // A `Service` is needed for every connection.
-    // This creates one from the `hello_world` function.
-    let make_svc = make_service_fn(|_conn| async {
-        Ok::<_, Infallible>(service_fn(echo))
+    // This creates one from the `route` function.
+    let make_svc = make_service_fn(move |_conn| {
+        // NOTE: the state must be cloned before use in an async block
+        let state = state.clone();
+        async move {
+        Ok::<_, Infallible>(service_fn(move |req| {
+            // NOTE: the state must be cloned AGAIN?
+            let state = state.clone();
+            async move {route(req, state).await}
+        }))
+        }
     });
 
     let server = Server::bind(&addr).serve(make_svc);
@@ -24,53 +33,35 @@ async fn main() {
     }
 }
 
-async fn echo(req: Request<Body>) -> Result<Response<Body>, hyper::Error> {
-    let mut response = Response::new(Body::empty());
+struct ServerState {
+    counter: AtomicU64,
+}
 
-    match (req.method(), req.uri().path()) {
-        (&Method::GET, "/") => {
+async fn route(req: Request<Body>, state: Arc<ServerState>) -> Result<Response<Body>, Infallible> {
+    state.counter.fetch_add(1, std::sync::atomic::Ordering::AcqRel);
+    let count = &state.counter;
+    let response = match (req.method(), req.uri().path()) {
+        (&Method::GET, _) => {
+            eprintln!("{count:?}, refs: {}\n {:#?}", Arc::strong_count(&state), req);
+            Response::builder()
+                .status(StatusCode::NOT_IMPLEMENTED)
+                .body(Body::from("not yet implemented"))
+                .unwrap()
+        },
+        (&Method::HEAD, _) => {
+            eprintln!("{count:?}, {:#?}", req);
+            Response::builder()
+                .status(StatusCode::NOT_IMPLEMENTED)
+                .body(Body::from("not yet implemented"))
+                .unwrap()
+        },
+        _ => {
             eprintln!("{:#?}", req);
-            *response.body_mut() = Body::from("Try POSTing data to /echo");
+            Response::builder()
+                .status(StatusCode::METHOD_NOT_ALLOWED)
+                .body(Body::from("Only get requests are possible"))
+                .unwrap()
         },
-        (&Method::POST, "/echo") => {
-            *response.body_mut() = req.into_body();
-        },
-        (&Method::POST, "/echo/reverse") => {
-            // Restrict large payloads
-            // NOTE(jladan): This line is different than in the tutorial, because `sizehint`
-            // returns a tuple rather than the `SizeHint` struct suggested by the docs
-            eprintln!("{:#?}", req);
-            let upper = req.body().size_hint().1
-                .unwrap_or(usize::MAX);
-            if upper > 1024 * 64 {
-                eprintln!("size hint: {:?}", req.body().size_hint());
-                let mut resp = Response::new(Body::from("Body too big"));
-                *resp.status_mut() = hyper::StatusCode::PAYLOAD_TOO_LARGE;
-                return Ok(resp);
-            }
-            // Await the full body
-            let full_body = hyper::body::to_bytes(req.into_body()).await?;
-
-            // reverse the bytes
-            let reversed = full_body.iter()
-                .rev()
-                .cloned()
-                .collect::<Vec<u8>>();
-
-            *response.body_mut() = reversed.into();
-        },
-        (&Method::POST, "/echo/uppercase") => {
-            let mapping = req
-                .into_body()
-                .map_ok(|chunk| {
-                    chunk.iter()
-                        .map(|byte| byte.to_ascii_uppercase())
-                        .collect::<Vec<u8>>()
-                });
-
-            *response.body_mut() = Body::wrap_stream(mapping);
-        },
-        _ => *response.status_mut() = StatusCode::NOT_FOUND,
     };
 
     Ok(response)
@@ -80,8 +71,4 @@ async fn shutdown_signal() {
     tokio::signal::ctrl_c()
         .await
         .expect("failed to install the CTRL+C signal handler");
-}
-
-async fn hello_wold(_req: Request<Body>) -> Result<Response<Body>, Infallible> {
-    Ok(Response::new("Hello, World".into()))
 }
